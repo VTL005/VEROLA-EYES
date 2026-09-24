9c3d911bc712b5848462b4b3cbdd48db58781cae
vythelong <longvt205@gmail.com>
2026-09-24 11:07:45 +0700

    fix: force https urls in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use App\Models\ChatConversation;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+        URL::forceScheme('https');
+                    }
         /*
         |--------------------------------------------------------------------------
         | ADMIN LAYOUT DATA
